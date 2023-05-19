9af472b8ea1c28367225f379a5fd43f0c55e9071
andreacassini <andrea1316@hotmail.it>
2023-05-19 10:15:15 +0200

    Fix button function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+
 let button = document.getElementById('submit');
 button.addEventListener('click', function(){
-    let inputName = document.getElementById('name').value;
-    let inputKm = document.getElementById('km').value;
-    let inputAge = document.getElementById('age').value;
-    console.log(inputName)
-    console.log(inputKm)
-    console.log(inputAge)
-})
+    let km = parseInt(document.getElementById('km').value);
+    let age = parseInt(document.getElementById('age').value);
+    
+    let price = km * 0.21;
+    let message = '';
+    if(isNaN(km) === false){
+
+        if(age === 0){
+            message += `Il prezzo del biglietto è di: ${price}€`;
+        }
+        else if(age === 1){
+            price *= 0.8;
+            message += `Il costo del tuo biglietto è scontato del 20%. Il prezzo è di: ${price.toFixed(2)}€`;
+        }
+        else{
+            price *= 0.6;
+            message += `Il costo del tuo biglietto è scontato del 40%. Il prezzo è di: ${price.toFixed(2)}€`;
+        }
+    }
+    else{
+        message += 'Devi inserire i km da percorrere'
+    }
+    
+    document.getElementById('price').innerText = message;
+});
+
