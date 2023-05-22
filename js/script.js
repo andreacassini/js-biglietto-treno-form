e69a82e64158b194087c97751618363683835b3b
andreacassini <andrea1316@hotmail.it>
2023-05-22 19:57:48 +0200

    STYLE and fix ticket

--- js/script.js
+++ js/script.js
@@ -1,29 +1,38 @@
 
 let button = document.getElementById('submit');
+document.getElementById('km').value = '';
+
 button.addEventListener('click', function(){
     let km = parseInt(document.getElementById('km').value);
     let age = parseInt(document.getElementById('age').value);
-    
+    let name = document.getElementById('name').value;
+    let cp_code = Math.floor(Math.random() * 10000) + 9999;
+    let rand_num = Math.floor(Math.random() * 12) + 1;
     let price = km * 0.21;
     let message = '';
+
     if(isNaN(km) === false){
 
         if(age === 0){
-            message += `Il prezzo del biglietto è di: ${price}€`;
+            
         }
         else if(age === 1){
             price *= 0.8;
-            message += `Il costo del tuo biglietto è scontato del 20%. Il prezzo è di: ${price.toFixed(2)}€`;
+
         }
         else{
             price *= 0.6;
-            message += `Il costo del tuo biglietto è scontato del 40%. Il prezzo è di: ${price.toFixed(2)}€`;
+
         }
     }
     else{
         message += 'Devi inserire i km da percorrere'
     }
     
-    document.getElementById('price').innerText = message;
+    document.getElementById('show-name').innerText = name;
+    document.getElementById('show-type').innerText = 'Biglietto Standard'
+    document.getElementById('show-rand-num').innerText = rand_num;
+    document.getElementById('show-cp').innerText = cp_code;
+    document.getElementById('show-price').innerText = price + ' €';
 });
 
